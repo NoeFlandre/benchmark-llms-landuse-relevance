c7853a180d5089c51721a7a5660897d2796f871c
NoeFlandre <noeflandre@gmail.com>
2026-09-20 23:08:29 +0200

    fix: reserve compatible Grid5000 GPUs

--- scripts/g5k_submit.sh
+++ scripts/g5k_submit.sh
@@ -13,6 +13,7 @@ for argument in "$@"; do
 done
 LRB_ROOT="${LRB_ROOT:-$HOME/benchmark-llms-landuse-relevance}"
 LRB_RESULTS="${LRB_RESULTS:-$LRB_ROOT/results-live}"
+LRB_GPU_FILTER="${LRB_GPU_FILTER:-gpu_compute_capability_major>=7 AND gpu_mem>=23040}"
 LRB_SHARD_INDEX="${LRB_SHARD_INDEX:-0}"
 LRB_SHARD_COUNT="${LRB_SHARD_COUNT:-1}"
 
@@ -25,7 +26,7 @@ fi
 
 usagepolicycheck -t
 
-oarsub -l "host=1/gpu=1,walltime=$WALLTIME" \
+oarsub -p "$LRB_GPU_FILTER" -l "host=1/gpu=1,walltime=$WALLTIME" \
   -O "$LRB_ROOT/oar.%jobid%.out" \
   -E "$LRB_ROOT/oar.%jobid%.err" \
   "LRB_ROOT='$LRB_ROOT' LRB_SHARD_INDEX=$LRB_SHARD_INDEX LRB_SHARD_COUNT=$LRB_SHARD_COUNT LRB_RESULTS='$LRB_RESULTS' $LRB_ROOT/scripts/g5k_node_run.sh"
